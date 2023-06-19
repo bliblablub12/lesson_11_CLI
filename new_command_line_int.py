14d10a3f5c1896d9ad24be94ac67cf98e5f2ba32
bliblablub12 <132387749+bliblablub12@users.noreply.github.com>
2023-06-19 20:03:32 +0200

    Add files via upload

---
+++ new_command_line_int.py
@@ -0,0 +1,31 @@
+import argparse
+
+poem_file = open('raven.txt', encoding='utf-8')
+content = poem_file.read()
+
+
+#vymena pisma za cislo
+parser = argparse.ArgumentParser (description='budeme delat zmeny v basni.')
+parser.add_argument("-i", "--input_file", type=str, help="input file", required=True)
+parser.add_argument("-v", "--vymena", help="replaces chosen letter with given number")
+parser.add_argument("-c", "--number", type=int, help="the number you wish to be in the text", required=True)
+parser.add_argument("-l", "--letter", type=str, help="the letter you wish to be replaced", required=True)
+args = parser.parse_args()
+
+input_file = args.input_file
+vymena = args.vymena
+number = args.number
+letter = args.letter
+
+def vymena(content, pismo, cislo):
+    with open('raven.txt', encoding='utf-8') as poem_file:
+        basen = poem_file.read()
+        nova_basen = basen.replace(pismo, str(cislo))
+        return nova_basen
+
+print(vymena(content, letter, number))
+
+
+
+
+
